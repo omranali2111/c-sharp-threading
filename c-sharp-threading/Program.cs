1f1dfaec6c007e72087195cd44e3887f772518de
omranali2111 <aljabri1234@hotmail.com>
2023-07-23 12:36:58 +0400

    implementing Race conditions

--- c-sharp-threading/Program.cs
+++ c-sharp-threading/Program.cs
@@ -1,32 +1,51 @@
 ﻿internal class Program
 {
+    static int value = 10;
     private static void Main(string[] args)
     {
+        /*
         Thread thread = new Thread(new ThreadStart(doSomething));
-        Console.WriteLine("ThreadState: {0}", thread.ThreadState);
+       
         thread.Start();
-        Console.WriteLine("ThreadState: {0}", thread.ThreadState);
-
+        
         for (int i = 0; i < 10; i++)
         {
             Console.WriteLine($"main thread:{i}");
             Thread.Sleep(10);
         }
-        Console.WriteLine("ThreadState: {0}", thread.ThreadState);
+       
         thread.Join();//it will wait other threads to fully execute 
-        Console.WriteLine("ThreadState: {0}", thread.ThreadState);
+        
 
         Console.WriteLine("Main block");//this will execute at the end 
-            
+        */
+        Thread thread1 = new Thread(incvalue);
+        Thread thread2 = new Thread(incvalue);
+        thread1.Start();
+        thread2.Start();
+        thread2.Join();
+        Console.WriteLine(value);
+
+
+
     }
 
     public static void doSomething()
     {
         for(int i = 0; i < 10; i++)
         {
-            Console.WriteLine($"worker thread:{i}");
+            Console.WriteLine($" thread:{i}");
             Thread.Sleep(100);
         }
 
     }
+
+  static void incvalue()
+    {
+        for( int i = 0;i < 10; i++)
+        {
+           value++;
+        }
+    }
+
 }
