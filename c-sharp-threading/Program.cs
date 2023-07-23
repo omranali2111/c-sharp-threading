54b933c37f58d4f2ccddea21127e4c00fa5196a2
omranali2111 <aljabri1234@hotmail.com>
2023-07-23 11:42:00 +0400

    checking the state of threads

--- c-sharp-threading/Program.cs
+++ c-sharp-threading/Program.cs
@@ -3,15 +3,18 @@
     private static void Main(string[] args)
     {
         Thread thread = new Thread(new ThreadStart(doSomething));
+        Console.WriteLine("ThreadState: {0}", thread.ThreadState);
         thread.Start();
+        Console.WriteLine("ThreadState: {0}", thread.ThreadState);
 
-        for(int i = 0; i < 10; i++)
+        for (int i = 0; i < 10; i++)
         {
             Console.WriteLine($"main thread:{i}");
             Thread.Sleep(10);
         }
-
+        Console.WriteLine("ThreadState: {0}", thread.ThreadState);
         thread.Join();//it will wait other threads to fully execute 
+        Console.WriteLine("ThreadState: {0}", thread.ThreadState);
 
         Console.WriteLine("Main block");//this will execute at the end 
             
