0ef7d951dfa34c30ba5f78687cfed27a2f71160d
omranali2111 <aljabri1234@hotmail.com>
2023-07-23 10:15:08 +0400

    c-sharp-threading

---
+++ c-sharp-threading/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Thread thread = new Thread(new ThreadStart(doSomething));
+        thread.Start();
+
+        for(int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"main thread:{i}");
+            Thread.Sleep(100);
+        }
+
+            
+    }
+
+    public static void doSomething()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"worker thread:{i}");
+            Thread.Sleep(100);
+        }
+
+    }
+}
